a3d2b6716e9afd85a343c4428fb0da0c37e60336
teyuu <tellini.matias@gmail.com>
2022-06-29 22:47:00 -0300

    hw 01 done

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,19 +1,33 @@
+/*
+  Lista de comandos
+------------------------
+  pwd -> imprime el directorio
+  date -> imprime la fecha actual
+  ls -> lista todos los archivos del directorio
+  echo -> me devuelve lo mismo que ingreso
+  cat -> devolver todo el contenido de un archivo
+  head -> devuelve las primeras lineas de un archivo
+  tail -> devuelve las ultimas lineas de un archivo
+  curl -> mostrar el contenido de una página
+*/
 
 const commands = require('./commands');
 
-
 // Output un prompt
 process.stdout.write('prompt > ');
+
 // El evento stdin 'data' se dispara cuando el user escribe una línea
+
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remueve la nueva línea
-  
+  var args = data.toString().trim().split(' '); // remueve la nueva línea
+  var cdm = args.shift();
+
   function write(data){
     process.stdout.write(data);
 
     process.stdout.write('\nprompt > ');
   }
-
-  commands[cmd](write)
+  if(!commands[cdm]){write('Comando no válido')}
+  else{commands[cdm](write, args)}
   
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,13 +1,50 @@
-function pwd(done){
-    done(process.cwd())
-}
-
-function date(done){
-    done(Date());  
-}
+var fs = require('fs')
+var axios = require('axios')
 
 module.exports = {
-    pwd,
-    date,
+
+    pwd: (done) => {
+        return done(process.cwd())
+    },
+    date: (done) =>{
+        done(Date())
+    },
+    ls: (done)=>{
+        return fs.readdir('.', 'utf-8', function(err, files){
+            if(err) throw err;
+            files.forEach((file) => {
+                done(file.toString() + '\n')
+            })
+        })
+    },
+    echo: (done, args) =>{ 
+        done(args.join(' ')); //utilizo join para concatar mi input que es un array
+    },
+    cat: (done, args) =>{
+        fs.readFile(args[0], 'utf-8', (err,data)=>{
+            if(err) throw err;
+            done(data);
+        })
+    },
+    head: (done,args) =>{
+        fs.readFile(args[0], 'utf-8', (err,data)=>{
+            if(err) throw err;
+            const lines =data.split('\n').slice(0, 5);
+            done(lines.join('\n'));
+        })
+    },
+    tail:(done,args) =>{
+        fs.readFile(args[0], 'utf-8', (err,data)=>{
+            if(err) throw err;
+            const lines =data.split('\n').slice(-5);
+            done(lines.join('\n'));
+        })
+    },
+    curl: (done,args)=>{
+        axios(args[0])
+        .then(res =>{
+            done(res.data.toString())
+        })
+    }
 };
 
